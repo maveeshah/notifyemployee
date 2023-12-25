47e513a0ae4fa144c5bbf58bf593bb49b729dd97
Ameer Muavia Shah <mavee.shah@hotmail.com>
2023-12-25 21:22:55 +0500

    fix: corrected message print

--- notifyemployee/events/auth.py
+++ notifyemployee/events/auth.py
@@ -1,7 +1,6 @@
 import frappe
 from frappe import _
-from frappe.utils.data import date_diff, today
-
+import time
 
 def successful_login(login_manager):
     """
@@ -13,8 +12,7 @@ def successful_login(login_manager):
         if employee:
             if not employee.person_to_be_contacted or not employee.emergency_phone_number \
                     or not employee.relation:
-                frappe.cache().hset("redirect_after_login", login_manager.user, f"app/employee/{employee.name}")
-                frappe.msgprint("Welcome to Frappe ERPNext! Please fill in your emergency contact details.")
+                frappe.msgprint("Please fill in your emergency contact details.")
 
             # if not employee.passport_number:
             #     frappe.local.response['type'] = 'redirect'
